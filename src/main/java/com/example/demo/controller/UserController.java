7bf21ea73532f689d78b06264d1d3b0dc8006b62
Madhawa Rathnayake <madhawasoftnet@gmail.com>
2024-12-17 22:18:42 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.UserDTO;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "api/v1/user")
+@CrossOrigin
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(value = "/getUsers")
+    public List<UserDTO> getUser(){
+        return userService.getAllUsers();
+    }
+    @PostMapping("/saveUser")
+    public UserDTO saveUser(@RequestBody UserDTO userDTO){
+        return userService.saveUser(userDTO);
+    }
+    @PutMapping("/updateUser")
+    public UserDTO updateUser(@RequestBody UserDTO userDTO) {
+        return userService.updateUser(userDTO);
+    }
+    @DeleteMapping("/deleteUser")
+    public boolean deleteUser(@RequestBody UserDTO userDTO){
+        return userService.deleteUser(userDTO);
+    }
+
+    @GetMapping("/getUserByUserID/{userID}")
+    public UserDTO getUserByUserID(@PathVariable String userID){ //PathVariable used because ID is taking from the url
+        return userService.getUserByUserID(userID);
+    }
+}
